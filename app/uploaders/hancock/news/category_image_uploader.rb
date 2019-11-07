b5c0a1a9acb9f60ac7e797c85207c3b4e8850c4d
Alexander Kiseliev <i43ack@gmail.com>
2019-11-07 14:40:29 +0300

    uploaders fix

--- app/uploaders/hancock/news/category_image_uploader.rb
+++
@@ -1,8 +0,0 @@
-if defined?(Shrine)
-  class Hancock::News::CategoryImageUploader < Shrine
-  
-    # include ::HancockShrine::UploaderBase
-    include ::HancockShrine::Uploadable
-
-  end
-end
--- app/uploaders/hancock/news/image_image_uploader.rb
+++
@@ -1,8 +0,0 @@
-if defined?(Shrine)
-  class Hancock::News::ImageImageUploader < Shrine
-  
-    include ::HancockShrine::BaseUploader
-    # include ::HancockShrine::Uploadable
-
-  end
-end
